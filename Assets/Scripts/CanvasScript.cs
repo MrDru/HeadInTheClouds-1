b43e1c68840b06a71204c43bcbc70ee48f6eb906
Nir Cafri <nircaf@gmail.com>
2023-10-13 11:32:53 +0300

    a

--- Assets/Scripts/CanvasScript.cs
+++ Assets/Scripts/CanvasScript.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
-public class CanvasScript : MonoBehaviour 
+public class CanvasScript : MonoBehaviour
 {
  public Text canvasText1;
-   void Start () 
+   void Start ()
    {
-      canvasText1.enabled = false; 
+      canvasText1.enabled = false;
    }
    void DisableText()
-   { 
-      canvasText1.enabled = false; 
-   }    
+   {
+      canvasText1.enabled = false;
+   }
    public void goodjob()
    {
-    canvasText1.enabled = true; 
+    canvasText1.enabled = true;
       Invoke("DisableText", 2f);//invoke after 5 seconds
    }
 }
